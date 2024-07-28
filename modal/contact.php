cb681251c8968d51f3f1d56add022a6069b62b92
jaswant singh <112382838+JaswantSinghBisht@users.noreply.github.com>
2024-07-28 21:10:50 +0530

    Update contact.php

--- modal/contact.php
+++ modal/contact.php
@@ -1,51 +1,54 @@
 <?php
+// Enable error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-// Put contacting email here
-$php_main_email = "jaswant93899@gmail.com";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Put contacting email here
+    $php_main_email = "jaswant93899@gmail.com";
 
-//Fetching Values from URL
-$php_name = $_POST['ajax_name'];
-$php_email = $_POST['ajax_email'];
-$php_message = $_POST['ajax_message'];
+    // Fetching Values from URL
+    $php_name = isset($_POST['ajax_name']) ? $_POST['ajax_name'] : '';
+    $php_email = isset($_POST['ajax_email']) ? $_POST['ajax_email'] : '';
+    $php_message = isset($_POST['ajax_message']) ? $_POST['ajax_message'] : '';
 
+    // Sanitizing email
+    $php_email = filter_var($php_email, FILTER_SANITIZE_EMAIL);
 
+    // After sanitization, validation is performed
+    if (filter_var($php_email, FILTER_VALIDATE_EMAIL)) {
+        $php_subject = "Message from contact form";
 
-//Sanitizing email
-$php_email = filter_var($php_email, FILTER_SANITIZE_EMAIL);
+        // To send HTML mail, the Content-type header must be set
+        $php_headers = 'MIME-Version: 1.0' . "\r\n";
+        $php_headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+        $php_headers .= 'From:' . $php_email . "\r\n"; // Sender's Email
+        $php_headers .= 'Cc:' . $php_email . "\r\n"; // Carbon copy to Sender
 
+        $php_template = '<div style="padding:50px;">Hello ' . $php_name . ',<br/>'
+        . 'Thank you for contacting us.<br/><br/>'
+        . '<strong style="color:#f00a77;">Name:</strong>  ' . $php_name . '<br/>'
+        . '<strong style="color:#f00a77;">Email:</strong>  ' . $php_email . '<br/>'
+        . '<strong style="color:#f00a77;">Message:</strong>  ' . $php_message . '<br/><br/>'
+        . 'This is a Contact Confirmation mail.'
+        . '<br/>'
+        . 'We will contact you as soon as possible.</div>';
 
-//After sanitization Validation is performed
-if (filter_var($php_email, FILTER_VALIDATE_EMAIL)) {
-	
-	
-		$php_subject = "Message from contact form";
-		
-		// To send HTML mail, the Content-type header must be set
-		$php_headers = 'MIME-Version: 1.0' . "\r\n";
-		$php_headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-		$php_headers .= 'From:' . $php_email. "\r\n"; // Sender's Email
-		$php_headers .= 'Cc:' . $php_email. "\r\n"; // Carbon copy to Sender
-		
-		$php_template = '<div style="padding:50px;">Hello ' . $php_name . ',<br/>'
-		. 'Thank you for contacting us.<br/><br/>'
-		. '<strong style="color:#f00a77;">Name:</strong>  ' . $php_name . '<br/>'
-		. '<strong style="color:#f00a77;">Email:</strong>  ' . $php_email . '<br/>'
-		. '<strong style="color:#f00a77;">Message:</strong>  ' . $php_message . '<br/><br/>'
-		. 'This is a Contact Confirmation mail.'
-		. '<br/>'
-		. 'We will contact you as soon as possible .</div>';
-		$php_sendmessage = "<div style=\"background-color:#f5f5f5; color:#333;\">" . $php_template . "</div>";
-		
-		// message lines should not exceed 70 characters (PHP rule), so wrap it
-		$php_sendmessage = wordwrap($php_sendmessage, 70);
-		
-		// Send mail by PHP Mail Function
-		mail($php_main_email, $php_subject, $php_sendmessage, $php_headers);
-		echo "";
-	
-	
+        $php_sendmessage = "<div style=\"background-color:#f5f5f5; color:#333;\">" . $php_template . "</div>";
+
+        // Message lines should not exceed 70 characters (PHP rule), so wrap it
+        $php_sendmessage = wordwrap($php_sendmessage, 70);
+
+        // Send mail by PHP Mail Function
+        if (mail($php_main_email, $php_subject, $php_sendmessage, $php_headers)) {
+            echo "Mail sent successfully.";
+        } else {
+            echo "Mail sending failed.";
+        }
+    } else {
+        echo "<span class='contact_error'>* Invalid email *</span>";
+    }
 } else {
-	echo "<span class='contact_error'>* Invalid email *</span>";
+    echo "Invalid request method.";
 }
-
 ?>
